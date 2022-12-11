d5a10067de6e8f5f1938affd177f65299f8fc1cb
PaulKnees <jakewallace368@gmail.com>
2022-12-11 20:05:26 +0000

    adds times

--- assets/script.js
+++ assets/script.js
@@ -1,7 +1,15 @@
 var currentDay = $('#currentDay');
 currentDay.text(moment().format('dddd, Do MMMM YYYY'))
+var clock = parseFloat(moment().format('HH'))
+console.log(clock)
 
+var nine = $('#9').text(moment().hours(9).format('HHA'));
+var ten = $('#10').text(moment().hours(10).format('HHA'));
+var elvn = $('#11').text(moment().hours(11).format('HHA'));
+var twlv = $('#12').text(moment().hours(12).format('HHA'));
+var one = $('#13').text(moment().hours(13).format('HHA'));
+var two = $('#14').text(moment().hours(14).format('HHA'));
+var three = $('#15').text(moment().hours(15).format('HHA'));
+var four = $('#16').text(moment().hours(16).format('HHA'));
+var five = $('#17').text(moment().hours(17).format('HHA'));
 
-var fullDay = $('.container');
-var hour = $('<div>').addClass('hour').text('howdy');
-fullDay.append(hour);
