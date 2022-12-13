9d3fc76ed70bac13353561bf484357b9c086933e
PaulKnees <jakewallace368@gmail.com>
2022-12-13 20:27:44 +0000

    successfully saves textarea INPUT to LS

--- assets/script.js
+++ assets/script.js
@@ -13,22 +13,102 @@ dayStart.add(1,'hours');
 }
 var row = $('.row');
 // append textarea and button
-row.append('<textarea>');
+row.append('<textarea name="text-area">');
 row.append('<button class="saveBtn">');
 // add icon to save button
 $('.saveBtn').append('<i class="far fa-save"></i>')
 
 
 
-// localStorage.getItem('key')
+// Individual rows
+var row9AM = container.children()[0];
+var row10AM = container.children()[1];
+var row11AM = container.children()[2];
+var row12PM = container.children()[3];
+var row1PM = container.children()[4];
+var row2PM = container.children()[5];
+var row3PM = container.children()[6];
+var row4PM = container.children()[7];
+var row5PM = container.children()[8];
+
+// console.log(row4PM, '4pm')
+
+
+
+// row9AM.children[1].text('text')
+
+
 $('button').click(function(){
-    console.log($(this).parent()[0].innerHTML)
-    // console.log($(this).parent()[0].innerHTML, 'this/parents')
-    console.log($('.row')[0].innerHTML, 'row 0')
-    if($(this).parent()[0].innerHTML == $('.row')[0].innerHTML){
-        localStorage.setItem('nineAM', 'nine AM')
-    } else {
-        alert('nope')
-    }
-//  localStorage.setItem('key', JSON.stringify($(this).prev()))
-})
+    if($(this).parent()[0].innerHTML == row9AM.innerHTML)  {
+var textbox = $(this).siblings()[1];
+// textbox.text() = 
+                /////////////////// This is the textarea
+        // console.log($(this).siblings()[1].val())
+
+
+        localStorage.setItem('9AM', $(this).siblings()[1].value)
+    // alert('9AM')
+
+}
+}
+)
+
+
+
+
+
+// if($(this).parent()[0].innerHTML == $('.row')[0].innerHTML){
+//     localStorage.setItem('nineAM', 'nine AM')
+// }
+
+// function nineAM(){
+  
+// }
+// function tenAM(){
+//     if($(this).parent()[0].innerHTML == $('.row')[1].innerHTML){
+//         localStorage.setItem('10AM', $(this).parent()[0].innerHTML)
+//     }
+// }
+// function elvnAM(){
+//     if($(this).parent()[0].innerHTML == $('.row')[2].innerHTML){
+//         localStorage.setItem('11AM', $(this).parent()[0].innerHTML)
+//     }
+// }
+// function twlvAM(){
+//     if($(this).parent()[0].innerHTML == $('.row')[3].innerHTML){
+//         localStorage.setItem('12AM', $(this).parent()[0].innerHTML)
+//     }
+// }
+// function onePM(){
+//     if($(this).parent()[0].innerHTML == $('.row')[4].innerHTML){
+//         localStorage.setItem('1PM', $(this).parent()[0].innerHTML)
+//     }
+// }
+// function twoPM(){
+//     if($(this).parent()[0].innerHTML == $('.row')[5].innerHTML){
+//         localStorage.setItem('2PM', $(this).parent()[0].innerHTML)
+//     }
+// }
+// function threePM(){
+//     if($(this).parent()[0].innerHTML == $('.row')[6].innerHTML){
+//         localStorage.setItem('3PM', $(this).parent()[0].innerHTML)
+//     }
+// }
+// function fourPM(){
+//     if($(this).parent()[0].innerHTML == $('.row')[7].innerHTML){
+//         localStorage.setItem('4PM', $(this).parent()[0].innerHTML)
+//     }
+// }
+// function fivePM(){
+//     if($(this).parent()[0].innerHTML == $('.row')[8].innerHTML){
+//         localStorage.setItem('5PM', $(this).parent()[0].innerHTML)
+//     }
+// }
+
+
+
+
+
+
+
+
