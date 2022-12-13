36b0b863fa39a94215f55beba5cba17682c4d390
PaulKnees <jakewallace368@gmail.com>
2022-12-13 18:03:47 +0000

    Specific button compares time to save specific key in localStorage (one working example)

--- assets/script.js
+++ assets/script.js
@@ -8,17 +8,27 @@ var dayStart = moment(09,'h');
 
 //Populate planner with rows
 while (dayStart.hour() < 18){
-    // container.append('<div class="row">')git add -
 container.append('<div class="row">' +   `<div class="hour"> ${dayStart.format('ha')} </div>`); 
 dayStart.add(1,'hours');
 }
 var row = $('.row');
+// append textarea and button
 row.append('<textarea>');
 row.append('<button class="saveBtn">');
-
+// add icon to save button
 $('.saveBtn').append('<i class="far fa-save"></i>')
 
+
+
+// localStorage.getItem('key')
 $('button').click(function(){
-//  console.log(this.prev())
+    console.log($(this).parent()[0].innerHTML)
+    // console.log($(this).parent()[0].innerHTML, 'this/parents')
+    console.log($('.row')[0].innerHTML, 'row 0')
+    if($(this).parent()[0].innerHTML == $('.row')[0].innerHTML){
+        localStorage.setItem('nineAM', 'nine AM')
+    } else {
+        alert('nope')
+    }
+//  localStorage.setItem('key', JSON.stringify($(this).prev()))
 })
-// btn.cick(alert('hgd'))
